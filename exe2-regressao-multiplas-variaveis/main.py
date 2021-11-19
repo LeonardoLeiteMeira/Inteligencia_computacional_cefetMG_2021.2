d297dedbec1d6b51676622e9aaa41042409ce9f3
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2021-11-19 11:27:57 -0300

    resolved q2
    Co-authored-by: Leonardo Leite Meira dos Santos <leonardo.meira@dtidigital.com.br>

--- exe2-regressao-multiplas-variaveis/main.py
+++ exe2-regressao-multiplas-variaveis/main.py
@@ -6,8 +6,6 @@ import random
 num_iterations = 2000
 alpha = 0.01
 
-def hypothesis_model(theta0, theta1, x):
-    return theta0 + (theta1 * x)
 
 def cost_function(X, y, theta):
     m = len(y)
@@ -16,39 +14,45 @@ def cost_function(X, y, theta):
     cost = 1/(2*m) * np.sum(squaredError)
     return cost
 
-def gradient_descendent(X, y, theta, alpha, num_iterations):
+
+def gradient_descendent(X, y, theta, alpha):
     m = len(y)
 
     hypothesis = np.sum(np.multiply(X, theta), axis=1)
     error = np.subtract(hypothesis, y)
     theta_new = alpha * 1/m * np.sum(np.multiply(X.T, error), axis=1)
     theta = np.subtract(theta, theta_new)
-    
+
     return theta
 
-df_city_profit = pd.read_csv("./data1.txt", sep=",", header=None)
-df_city_profit.columns = ['population', 'profit']
+
+columns = ['population', 'profit']
+df_city_profit = pd.read_csv(
+    "./data1.txt", sep=",", header=None, names=columns)
 print(df_city_profit)
 
-m = len(df_city_profit.profit)
+m = len(df_city_profit['profit'])
 theta0 = random.randint(1, 25)
 theta1 = random.randint(1, 25)
 
-theta = np.asarray([theta0,theta1]).astype(float)
-x = df_city_profit.population.to_numpy()
+theta = np.asarray([theta0, theta1]).astype(float)
+x = df_city_profit['population'].to_numpy()
 X = np.vstack((np.ones(m), x.T)).T
-y = df_city_profit.profit.to_numpy()
+y = df_city_profit['profit'].to_numpy()
 cost_array = []
 hypothesis_array = []
 
 for i in range(num_iterations):
     hypothesis_array.append(np.sum(np.multiply(X, theta), axis=1))
-    theta = gradient_descendent(X, y, theta, alpha, num_iterations)
+    theta = gradient_descendent(X, y, theta, alpha)
     cost_array.append(cost_function(X, y, theta))
 
-plt.plot(cost_array[0:len(cost_array)],color='blue', linewidth=1)
+plt.plot(cost_array[0:len(cost_array)], color='blue', linewidth=1)
+plt.xlabel("iteracoes")
+plt.ylabel("custo")
 plt.show()
 
 df_city_profit.plot.scatter(x="population", y="profit")
-plt.plot(X[:,[1]], np.sum(np.multiply(X,theta), axis=1), color='red', linewidth=1)
+plt.plot(X[:, [1]], np.sum(np.multiply(X, theta), axis=1),
+         color='red', linewidth=1)
 plt.show()
--- exe2-regressao-multiplas-variaveis/q2.py
+++ exe2-regressao-multiplas-variaveis/q2.py
@@ -4,38 +4,98 @@ import numpy as np
 import random
 
 num_iterations = 2000
-alpha = 0.01
 
 
 def normalization_of_x(x, average, standard_deviation):
     return (x-average)/standard_deviation
 
+
 def normalize_column(column):
     average = np.average(column)
     standard_deviation_value = np.std(column)
     size_of_column = len(column)
-    new_array = [ normalization_of_x(column[index],average,standard_deviation_value) for index in range(0,size_of_column) ]
+    new_array = [normalization_of_x(
+        column[index], average, standard_deviation_value) for index in range(0, size_of_column)]
     return np.array(new_array)
+
 
 def normalize_df(df):
     new_size_column = normalize_column(df['size'])
     new_bedrooms_column = normalize_column(df['bedrooms'])
     new_price_column = normalize_column(df['price'])
-    new_df = pd.DataFrame({'size': new_size_column, 'bedrooms' : new_bedrooms_column, 'price': new_price_column })
+    new_df = pd.DataFrame(
+        {'size': new_size_column, 'bedrooms': new_bedrooms_column, 'price': new_price_column})
     new_df.columns = ['size', 'bedrooms', 'price']
     return new_df
 
+
+def cost_function(X, y, theta):
+    m = len(y)
+    hypothesis = np.sum(np.multiply(X, theta), axis=1)
+    squaredError = np.power(np.subtract(hypothesis, y), 2)
+    cost = 1/(2*m) * np.sum(squaredError)
+    return cost
+
+
+def gradient_descendent(X, y, theta, alpha):
+    m = len(y)
+
+    hypothesis = np.sum(np.multiply(X, theta), axis=1)
+    error = np.subtract(hypothesis, y)
+    theta_new = alpha * 1/m * np.sum(np.multiply(X.T, error), axis=1)
+    theta = np.subtract(theta, theta_new)
+
+    return theta
+
+
+def execute(alpha):
+    m = len(normalized_df['price'])
+    theta0 = random.randint(1, 25)
+    theta1 = random.randint(1, 25)
+    theta2 = random.randint(1, 25)
+
+    theta = np.asarray([theta0, theta1, theta2]).astype(float)
+    x1 = normalized_df['size'].to_numpy()
+    x2 = normalized_df['bedrooms'].to_numpy()
+    X = np.vstack((x1, x2))
+    X = np.vstack((np.ones(m), X)).T
+    y = normalized_df['price'].to_numpy()
+    cost_array = []
+    hypothesis_array = []
+
+    for i in range(num_iterations):
+        hypothesis_array.append(np.sum(np.multiply(X, theta), axis=1))
+        theta = gradient_descendent(X, y, theta, alpha)
+        cost_array.append(cost_function(X, y, theta))
+
+    # plt.plot(cost_array[0:len(cost_array)], color='blue', linewidth=1)
+    # plt.xlabel("iteracoes")
+    # plt.ylabel("custo")
+    # plt.show()
+    return cost_array[0:len(cost_array)]
+
+
 columns = ['size', 'bedrooms', 'price']
-df_house_price = pd.read_csv("./data2.txt", sep=",", header=None, names=columns)
-print(df_house_price)
+df_house_price = pd.read_csv(
+    "./data2.txt", sep=",", header=None, names=columns)
 
-print("-----------")
+normalized_df = normalize_df(df_house_price)
 
-print(normalize_df(df_house_price))
-
-
+print(normalized_df)
 
 
+alpha = [0.001, 0.01, 0.05, 0.1, 1]
+colors = ['blue', 'red', 'green', 'yellow', 'purple']
 
+for i in range(0, len(alpha)):
+    plt.plot(execute(alpha[i]), color=colors[i],
+             linewidth=1, label=f"alpha={alpha[i]}")
 
+plt.xlabel("iteracoes")
+plt.ylabel("custo")
+plt.legend()
+plt.show()
 
+# -----------------------------------------------------------
+
+# 2.3 -> Não é possivel traçar o ajuste linear, pois agora temos multiplas dimensões
