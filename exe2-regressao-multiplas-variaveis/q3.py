edccec392af27a67d7a05308057d15daa37bb65d
Rafael Lopes <rafaelsilveriodesalopes@gmail.com>
2021-11-21 16:27:11 -0300

    final jupyter notebook

--- exe2-regressao-multiplas-variaveis/q3.py
+++ exe2-regressao-multiplas-variaveis/q3.py
@@ -5,20 +5,22 @@ import pandas as pd
 import numpy as np
 import random
 
+
 def medium_squared_error(y, predicted, m):
     squaredError = np.power(np.subtract(y, predicted), 2)
     return 1/(m) * np.sum(squaredError)
+
 
 columns = ['size', 'bedrooms', 'price']
 df_house_price = pd.read_csv(
     "./data2.txt", sep=",", header=None, names=columns)
 
 column_size = df_house_price['size'].to_numpy()
-column_bedrooms =  df_house_price['bedrooms'].to_numpy()
+column_bedrooms = df_house_price['bedrooms'].to_numpy()
 
 m = len(df_house_price['price'])
 
-matrix_Y =  df_house_price['price'].to_numpy()
+matrix_Y = df_house_price['price'].to_numpy()
 matrix_X = np.vstack((column_size, column_bedrooms))
 X = np.vstack((np.ones(m), matrix_X)).T
 
